e1facb7762f9254d5d1f590d72fa1e499dccf42f
Krinickij <krinickij.v.a@gmail.com>
2023-02-14 14:20:04 +0300

    added sliders for inputs and slyles

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,3 +1,46 @@
-$( document ).ready(function() {
-    $( "#slider" ).slider();
+
+
+$(document).ready(function () {
+    $( function() {
+        $( "#js-slider-cost-min" ).slider({
+            range: "min",
+            value: 15,
+            min: 1,
+            max: 200,
+            slide: function( event, ui ) {
+                $( "#js-cost-rub" ).val( ui.value );
+            }
+        });
+
+        $( "#js-cost-rub" ).val($("#js-slider-cost-min" ).slider( "value" ) );
+    } );
+    // $('.calculator__input_cost').keyup(function (){
+    //     let inputValue = $('.calculator__input_cost').val()
+    //     $("#js-slider-cost-min" ).slider( "value", inputValue)
+    // })
+
+    $( function() {
+        $( "#js-slider-deposit" ).slider({
+            range: "min",
+            value: 42000,
+            min: 1,
+            max: 200000,
+            slide: function( event, ui ) {
+                $( "#js-calculator-lising" ).val( ui.value + " ₽");
+            }
+        });
+        $( "#js-calculator-lising" ).val( $( "#js-slider-deposit" ).slider( "value" ) + " ₽");
+    } );
+    $( function() {
+        $( "#js-slider-month" ).slider({
+            range: "min",
+            value: 60,
+            min: 1,
+            max: 200,
+            slide: function( event, ui ) {
+                $( "#js-calculator-month" ).val(ui.value );
+            }
+        });
+        $( "#js-calculator-month" ).val($( "#js-slider-month" ).slider( "value" ) );
+    } );
 });
